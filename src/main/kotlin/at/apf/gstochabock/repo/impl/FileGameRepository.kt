408b2c9f47ccda132407a4f8fc11f154ef9e1f95
schokobaer <andreas.peter.fend@gmail.com>
2021-01-15 20:40:20 +0100

    Implemented FileGameRepository

--- src/main/kotlin/at/apf/gstochabock/repo/impl/FileGameRepository.kt
+++ src/main/kotlin/at/apf/gstochabock/repo/impl/FileGameRepository.kt
@@ -2,10 +2,18 @@ package at.apf.gstochabock.repo.impl
 
 import at.apf.gstochabock.model.Table
 import at.apf.gstochabock.repo.GameRepository
+import at.apf.gstochabock.serialize.TableSerDes
 import at.apf.gstochabock.util.IdGenerator
+import org.apache.commons.io.FileUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Repository
+import java.io.File
+import java.io.IOException
+import java.io.PrintWriter
+import java.lang.RuntimeException
+import java.nio.file.Files
 import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantLock
 
@@ -17,6 +25,12 @@ class FileGameRepository : GameRepository {
 
     @Autowired
     private lateinit var idGenerator: IdGenerator
+
+    @Autowired
+    private lateinit var serDes: TableSerDes
+
+    @Value("game.directory")
+    private lateinit var directory: String
 
     override fun createTestTable(table: Table) {
         TODO("Not yet implemented")
@@ -30,38 +44,69 @@ class FileGameRepository : GameRepository {
             locks[id]!!.lock()
         }
 
-        // write file
+        writeFile(table)
 
-        locks[id]!!.unlock()
+        unlock(id)
         return id
     }
 
     override fun list(pred: (Table) -> Boolean): List<Table> {
-        TODO("Not yet implemented")
+        return File(directory).listFiles()
+                .filter { it.endsWith(".json") && it.isFile }
+                .map { it.name.removeSuffix(".json") }
+                .map { readFile(it) }
     }
 
     override fun read(id: String): Table {
-        TODO("Not yet implemented")
+        return readFile(id)
     }
 
     override fun lockedRead(id: String): Table {
-        TODO("Not yet implemented")
+        synchronized(locks) {
+            if (!locks.containsKey(id)) {
+                throw RuntimeException("No table with id $id")
+            }
+            locks[id]!!.lock()
+        }
+
+        try {
+            return readFile(id)
+        } catch (e: IOException) {
+            unlock(id)
+            throw RuntimeException(e)
+        }
     }
 
     override fun unlock(id: String) {
-        TODO("Not yet implemented")
+        synchronized(locks) {
+            if (!locks.containsKey(id)) {
+                throw RuntimeException("No table with id $id")
+            }
+            locks[id]!!.unlock()
+        }
     }
 
     override fun writeBack(table: Table) {
-        TODO("Not yet implemented")
+        try {
+            writeFile(table)
+        } finally {
+            unlock(table.id)
+        }
     }
 
     private fun writeFile(table: Table) {
-
+        val json = serDes.toText(table)
+        FileUtils.write(File(directory + table.id + ".json"), json, "UTF-8")
     }
 
     private fun readFile(tableid: String): Table {
-        TODO("Read table and convert")
+        val json = FileUtils.readFileToString(File(directory + tableid + ".json"), "UTF-8")
+        val table = serDes.fromText(json)
+        table.id = tableid
+        if (table.history !== null) {
+            table.history!!.id = tableid
+        }
+        return table
     }
 
 }
---
+++ src/main/kotlin/at/apf/gstochabock/serialize/TableSerDes.kt
@@ -0,0 +1,51 @@
+package at.apf.gstochabock.serialize
+
+import at.apf.gstochabock.gamelogic.BaseJassLogic
+import at.apf.gstochabock.gamelogic.JassLogic
+import at.apf.gstochabock.model.*
+import com.google.gson.Gson
+import org.springframework.stereotype.Component
+
+@Component
+class TableSerDes {
+
+    private val gson = Gson()
+
+    fun toText(table: Table): String {
+        val tdo = TableDO(table.password, table.points, table.weisPoints, table.currentMove, table.trumpf, table.round, table.roundHistory, table.players, null, table.logic.serializationCode(), table.state)
+        val history = table.history
+
+        if (history !== null) {
+            tdo.history = TableDO(history.password, history.points, history.weisPoints, history.currentMove, history.trumpf, history.round, history.roundHistory, history.players, null, history.logic.serializationCode(), history.state)
+        }
+
+        return gson.toJson(tdo)
+    }
+
+    fun fromText(text: String): Table {
+        val tdo = gson.fromJson<TableDO>(text, Table::class.java)
+        val logic = BaseJassLogic()
+        val table = Table("", tdo.password, tdo.points, tdo.weisPoints, tdo.currentMove, tdo.trumpf, tdo.round, tdo.roundHistory, tdo.players, null, logic, tdo.state)
+        val h = tdo.history
+        if (h !== null) {
+            table.history = Table("", h.password, h.points, h.weisPoints, h.currentMove, h.trumpf, h.round, h.roundHistory, h.players, null, logic, h.state)
+        }
+
+        return table
+    }
+}
+
+
+private data class TableDO(
+        var password: String?,
+        var points: MutableList<Int>,
+        var weisPoints: MutableList<Int>,
+        var currentMove: Int?,
+        var trumpf: Trumpf?,
+        var round: MutableList<Card>,
+        val roundHistory: MutableList<RoundHistory>,
+        var players: MutableList<Player>,
+        var history: TableDO?,
+        val logic: String,
+        var state: TableState = TableState.PENDING
+)
