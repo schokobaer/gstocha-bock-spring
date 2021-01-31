bd82307cc54203217da1bec25f00c02ca0586dee
schokobaer <andreasfend@msn.com>
2021-01-31 11:42:05 +0100

    introduced vbg table names

--- src/main/kotlin/at/apf/gstochabock/util/IdGenerator.kt
+++ src/main/kotlin/at/apf/gstochabock/util/IdGenerator.kt
@@ -2,11 +2,51 @@ package at.apf.gstochabock.util
 
 import org.springframework.stereotype.Component
 import java.util.*
+import kotlin.random.Random
 
 @Component
 class IdGenerator {
 
+    val words = listOf(
+
+        "käeknöpfle",
+        "riebelbibel",
+        "funka",
+
+        "stöcke",
+        "slalom",
+        "trumpf",
+
+        "fläschafurzer",
+        "doppelfisch",
+        "strohkopf",
+        "elefant",
+        "bluamatopf",
+        "weli",
+        "löwe",
+        "schlange",
+
+        "seebrünzler",
+        "oberländer",
+        "nüziders",
+        "ems",
+        "breagaz",
+        "dorabira",
+        "luschnau",
+        "bludaz",
+        "feldkirch",
+        "montafu",
+        "wald"
+    )
+
+    val rnd = Random(Date().time)
+
     fun nextId(): String {
-        return UUID.randomUUID().toString()
+        val word = words[rnd.nextInt(words.size)]
+        var code = ""
+        for (i in 1..4) {
+            code += rnd.nextInt(10).toString()
+        }
+        return "$word-$code"
     }
 }
