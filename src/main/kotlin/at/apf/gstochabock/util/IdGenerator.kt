852270613ea31642340452a7c161de19d2121dd9
schokobaer <andreasfend@msn.com>
2021-02-07 11:57:36 +0100

    Secured IdGenerator with exist checking

--- src/main/kotlin/at/apf/gstochabock/util/IdGenerator.kt
+++ src/main/kotlin/at/apf/gstochabock/util/IdGenerator.kt
@@ -1,6 +1,9 @@
 package at.apf.gstochabock.util
 
+import at.apf.gstochabock.repo.GameRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.io.IOException
 import java.util.*
 import kotlin.random.Random
 
@@ -41,12 +44,28 @@ class IdGenerator {
 
     val rnd = Random(Date().time)
 
+    @Autowired
+    private lateinit var repo: GameRepository
+
     fun nextId(): String {
-        val word = words[rnd.nextInt(words.size)]
-        var code = ""
-        for (i in 1..4) {
-            code += rnd.nextInt(10).toString()
+        var id = ""
+        do {
+            val word = words[rnd.nextInt(words.size)]
+            var code = ""
+            for (i in 1..4) {
+                code += rnd.nextInt(10).toString()
+            }
+            id = "$word-$code"
+        } while (exists(id))
+        return id
+    }
+
+    fun exists(id: String): Boolean {
+        return try {
+            repo.read(id)
+            true
+        } catch (e: Exception) {
+            false
         }
-        return "$word-$code"
     }
 }
